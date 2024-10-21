5e1ca7570c1fd96bf1800f2fe70451b5049fe4c3
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-10-21 09:51:40 +0200

    code cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  base: "./",
   plugins: [
     civet({ ts: "preserve" }),
     solid()
